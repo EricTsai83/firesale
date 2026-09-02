0f0f8a97c31ef2107acd6788553be57196280164
Eric Tsai <eric492718@gmail.com>
2026-09-02 23:17:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+[![Frontend Masters](https://static.frontendmasters.com/assets/brand/logos/full.png)][fem]
+
+# Firesale
+
+This is a companion repo for the [Electron-v3][course] course on [Frontend Masters][fem].
+
+## Setup
+Make sure you have [Node.js](https://nodejs.org/) v18.12 or newer, [Git](https://git-scm.com/), and [pnpm](https://pnpm.io/) installed.
+
+Run `corepack enable` to enable pnpm through Corepack.
+
+The following commands will install the repo dependencies and start the Firesale application.
+```
+pnpm install
+pnpm start
+```
+
+[fem]: https://frontendmasters.com
+[course]: https://frontendmasters.com/courses/electron-v3/
---
+++ forge.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-vite',
+      config: {
+        build: [
+          {
+            entry: 'src/main/index.ts',
+            config: 'vite.main.config.mjs',
+            target: 'main',
+          },
+          {
+            entry: 'src/preload.ts',
+            config: 'vite.preload.config.mjs',
+            target: 'preload',
+          },
+        ],
+        renderer: [
+          {
+            name: 'main_window',
+            config: 'vite.renderer.config.mjs',
+          },
+        ],
+      },
+    },
+  ],
+};
