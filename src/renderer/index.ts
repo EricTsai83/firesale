48aa3c60131ef038917fa312ac260d05b9a1baae
Eric Tsai <eric492718@gmail.com>
2026-09-04 18:42:55 +0800

    feat: implement saveMarkdown functionality to allow saving Markdown content via the new saveFile API

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -19,3 +19,10 @@ Elements.ExportHtmlButton.addEventListener("click", () => {
   const html = Elements.RenderedView.innerHTML;
   window.api.showExportHtmlDialog(html);
 });
+
+Elements.SaveMarkdownButton.addEventListener("click", async () => {
+  const content = Elements.MarkdownView.value;
+  window.api.saveFile(content);
+});
+
+Elements.SaveMarkdownButton.disabled = false;
